380bc6fc13de8a0dd5b60f8d7a37c0cc4c97eeec
ishihara <ishikortakkun@gmail.com>
2025-07-13 15:11:07 +0900

    掴みっぽい動き作った

--- ros2_ws/src/joint_pub/joint_pub/arm_via_description.py
+++ ros2_ws/src/joint_pub/joint_pub/arm_via_description.py
@@ -1,8 +1,13 @@
 from math import pi
+from math import radians
 
 arm_via = [
     [2, [0, 0, 0, 0, 0, 0]],  # 初期位置 [period[s], [q0, q1, q2, q3, q4, q5]]
-    [3, [pi / 2, pi / 2, pi / 2, pi / 2, pi / 2, pi / 2]],
-    [10, [-pi / 2, -pi / 2, -pi / 2, -pi / 2, -pi / 2, -pi / 2]],
-    [10, [0, 0, 0, 0, 0, 0]],
+    [1, [0, radians(-60), radians(150), radians(-150), 0, 0]],
+    [1, [0, radians(-60), radians(150), radians(-150), 0, 0]],
+    [3, [radians(-75), -pi / 6, pi / 2, 0, radians(105), 0]],
+    [3, [radians(-75), -pi / 6, pi / 2, 0, radians(105), 0]],
+    [3, [radians(-105), -pi / 6, pi / 2, 0, radians(80), 0]],
+    [3, [radians(-105), -pi / 6, pi / 2, 0, radians(80), 0]],
+    [2, [0, 0, 0, 0, 0, 0]],  # 初期位置 [period[s], [q0, q1, q2, q3, q4, q5]]
 ]

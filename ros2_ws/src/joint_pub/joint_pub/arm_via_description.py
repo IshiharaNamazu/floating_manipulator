ac26d5632ffea0fb07b5f0aa0ae81eb84b16d695
ishihara <ishikortakkun@gmail.com>
2025-07-13 21:24:12 +0900

    sinの速度軌道作った

--- ros2_ws/src/joint_pub/joint_pub/arm_via_description.py
+++ ros2_ws/src/joint_pub/joint_pub/arm_via_description.py
@@ -2,12 +2,11 @@ from math import pi
 from math import radians
 
 arm_via = [
-    [2, [0, 0, 0, 0, 0, 0]],  # 初期位置 [period[s], [q0, q1, q2, q3, q4, q5]]
-    [1, [0, radians(-60), radians(150), radians(-150), 0, 0]],
-    [1, [0, radians(-60), radians(150), radians(-150), 0, 0]],
-    [3, [radians(-75), -pi / 6, pi / 2, 0, radians(100), radians(60)]],
+    [3, [0, 0, 0, 0, 0, 0]],  # 初期位置 [period[s], [q0, q1, q2, q3, q4, q5]]
+    [3, [0, radians(-60), radians(150), radians(-150), 0, 0]],
+    [3, [0, radians(-60), radians(150), radians(-150), 0, 0]],
     [3, [radians(-75), -pi / 6, pi / 2, 0, radians(100), radians(60)]],
     [3, [radians(-105), -pi / 6, pi / 2, 0, radians(80), radians(60)]],
     [3, [radians(-105), -pi / 6, pi / 2, 0, radians(80), radians(60)]],
-    [2, [0, 0, 0, 0, 0, 0]],  # 初期位置 [period[s], [q0, q1, q2, q3, q4, q5]]
+    [3, [0, 0, 0, 0, 0, 0]],  # 初期位置 [period[s], [q0, q1, q2, q3, q4, q5]]
 ]
--- ros2_ws/src/joint_pub/joint_pub/play_trajectory.py
+++ ros2_ws/src/joint_pub/joint_pub/play_trajectory.py
@@ -1,6 +1,7 @@
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Float64
+from math import pi, sin
 
 from .arm_via_description import arm_via
 
@@ -14,9 +15,7 @@ class PlayTrajectory(Node):
             topic = f"/floating_manipulator/joint{i}"
             pub = self.create_publisher(Float64, topic, 10)
             self.joint_publishers_.append(pub)
-        self.timer_ = self.create_timer(
-            0.001, self.publish_joint_velocity_linear
-        )  # 20Hz
+        self.timer_ = self.create_timer(0.001, self.publish_joint_velocity_sin)  # 20Hz
         self.pre_log_time_ = self.get_clock().now()
         self.via_start_time_ = self.get_clock().now()
 
@@ -27,6 +26,48 @@ class PlayTrajectory(Node):
         ):
             self.pre_log_time_ = self.get_clock().now()
             self.get_logger().info(info_str)
+
+    def publish_joint_velocity_sin(self):
+        # viaの時間が経過したら次のviaへ
+        if self.via_index_ >= len(arm_via) - 1:  # 最後のviaに到達した場合終了
+            if self.get_clock().now() >= self.via_start_time_ + rclpy.duration.Duration(
+                seconds=arm_via[-1][0]
+            ):
+                self.via_index_ = len(arm_via)
+                self.info_throttle("end of trajectory")
+        else:
+            if self.get_clock().now() >= self.via_start_time_ + rclpy.duration.Duration(
+                seconds=arm_via[self.via_index_][0]
+            ):
+                self.via_start_time_ += rclpy.duration.Duration(
+                    seconds=arm_via[self.via_index_][0]
+                )
+                self.via_index_ += 1
+                self.get_logger().info("next_via_index:" + str(self.via_index_))
+
+        msgs = []
+        if (self.via_index_ == 0) or (self.via_index_ >= len(arm_via)):
+            for i in range(len(arm_via[0][1])):
+                msg = Float64()
+                msg.data = 0.0
+                msgs.append(msg)
+        else:
+            for i in range(len(arm_via[self.via_index_][1])):
+                msg = Float64()
+                d = arm_via[self.via_index_][1][i] - arm_via[self.via_index_ - 1][1][i]
+                omega = pi / arm_via[self.via_index_][0]  # ω = π / T
+                t = (self.get_clock().now() - self.via_start_time_).nanoseconds / 1e9
+                v = (d / 2) * omega * sin(omega * t)  # v=d * ω * sin(ωt)
+                msg.data = v
+                msgs.append(msg)
+
+        log_str = f"index:{self.via_index_}, publish joint velocity: "
+        for i, msg in enumerate(msgs):
+            log_str += f"joint{i}: {msg.data:.3f}, "
+        self.info_throttle(log_str)
+
+        for i, pub in enumerate(self.joint_publishers_):
+            pub.publish(msgs[i])
 
     def publish_joint_velocity_linear(self):
         # viaの時間が経過したら次のviaへ

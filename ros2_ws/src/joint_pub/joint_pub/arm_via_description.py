c303f3e9940df0c248121dbeae22a0b04f4f3bf3
ishihara <ishikortakkun@gmail.com>
2025-07-13 15:31:01 +0900

    関節軌道微修正

--- ros2_ws/src/joint_pub/joint_pub/arm_via_description.py
+++ ros2_ws/src/joint_pub/joint_pub/arm_via_description.py
@@ -5,8 +5,8 @@ arm_via = [
     [2, [0, 0, 0, 0, 0, 0]],  # 初期位置 [period[s], [q0, q1, q2, q3, q4, q5]]
     [1, [0, radians(-60), radians(150), radians(-150), 0, 0]],
     [1, [0, radians(-60), radians(150), radians(-150), 0, 0]],
-    [3, [radians(-75), -pi / 6, pi / 2, 0, radians(105), 0]],
-    [3, [radians(-75), -pi / 6, pi / 2, 0, radians(105), 0]],
+    [3, [radians(-75), -pi / 6, pi / 2, 0, radians(100), 0]],
+    [3, [radians(-75), -pi / 6, pi / 2, 0, radians(100), 0]],
     [3, [radians(-105), -pi / 6, pi / 2, 0, radians(80), 0]],
     [3, [radians(-105), -pi / 6, pi / 2, 0, radians(80), 0]],
     [2, [0, 0, 0, 0, 0, 0]],  # 初期位置 [period[s], [q0, q1, q2, q3, q4, q5]]
